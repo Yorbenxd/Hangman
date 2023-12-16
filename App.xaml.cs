270c5659bcad4ec51a7886ae47ac4c0b95e0c407
Yorben <115065871+Yorbenxd@users.noreply.github.com>
2023-12-16 14:24:32 +0100

    Added Register/Login
    Added a register/login system using SQLite

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Hangman
 {
@@ -13,5 +14,12 @@ namespace Hangman
 	/// </summary>
 	public partial class App : Application
 	{
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			//base.OnStartup(e);
+			DatabaseFacade facade = new DatabaseFacade(new UserDataContext());
+			facade.EnsureCreated();
+		}
 	}
 }
---
+++ UserDataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Hangman
+{
+    public class UserDataContext:DbContext
+    {
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlite("Data Source = DataFile.db");
+		}
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
